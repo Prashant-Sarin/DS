06ded44a25888f27be48e1f3fe9f7fbb10dd13e8
Prashant Sarin <prasht.sarin@gmail.com>
2018-01-15 17:15:51 +0530

    after swaping nodes

--- src/com/sarindev/ds/linkedlist/LinkedList.java
+++ src/com/sarindev/ds/linkedlist/LinkedList.java
@@ -69,10 +69,10 @@ public class LinkedList<T> {
 			for (int i = 0; i < position - 1; i++) {
 				temp = temp.next;
 			}
-			System.out.println("temp  = " + temp.data);
 
 			n.next = temp.next;
 			temp.next = n;
+			length++;
 		}
 
 	}
@@ -82,21 +82,160 @@ public class LinkedList<T> {
 		Node<T> previous = null;
 		Node<T> temp = head;
 
-		if(head.data == data) {
-			head=head.next;
+		if (head.data == data) {
+			head = head.next;
+			length--;
 			return;
-		}else {
-			while (temp.next!=null) {
-				previous=temp;
-				temp=temp.next;
+		} else {
+			while (temp.next != null) {
+				previous = temp;
+				temp = temp.next;
 				if (temp.data == n.data) {
-					previous.next =temp.next;
-					temp.next=null;
+					previous.next = temp.next;
+					temp.next = null;
+					length--;
 					return;
 				}
 			}
 		}
-				
+
+	}
+
+	public void reverse() {
+		Node<T> temp = head;
+		// Node<T> temp1=head;
+		Node<T> current = null;
+		Node<T> previous = null;
+		while (temp != null) {
+			current = temp;
+			temp = current.next;
+			current.next = previous;
+			previous = current;
+		}
+		head = previous;
+	}
+
+	// public void reverse() {
+	// Node<T> temp = head;
+	// LinkedList<T> reverseList = new LinkedList<T>();
+	// while(temp!=null) {
+	// reverseList.insertAtFirst(temp.data);
+	// temp= temp.next;
+	// }
+	// this.head = reverseList.head;
+	// }
+
+	public T nFromLast(int n) {
+		Node<T> node = null;
+		int diff = length - n;
+		// Node<T> previous=null;
+		Node<T> current = head;
+		if (diff < 0) {
+			return null;
+		} else {
+			int count = 0;
+			while (current != null && count != diff) {
+				current = current.next;
+				count++;
+			}
+			node = current;
+		}
+		return node.data;
+	}
+
+	public int getLength() {
+		if (head == null)
+			return 0;
+		else {
+			head = head.next;
+			return 1 + getLength();
+		}
+		// return length;
+	}
+
+	public boolean contains(T data) {
+		Node<T> current = head;
+		while (current != null) {
+			// System.out.println("current.data = "+current.data);
+			if (current.data == data)
+				return true;
+			current = current.next;
+		}
+		return false;
+	}
+
+	public void swap(T data1, T data2) {
+		Node<T> node1 = null;
+		Node<T> node2 = null;
+		Node<T> previous1 = null;
+		Node<T> previous2 = null;
+		Node<T> next1 = null;
+		Node<T> next2 = null;
+		Node<T> previous = null;
+		Node<T> current = head;
+		boolean found1 = false;
+		boolean found2 = false;
+		while ((current != null) && !(found1 && found2)) {
+			if (current.data == data1) {
+				found1 = true;
+				node1 = current;
+				previous1 = previous;
+				System.out.println("Node1  = " + node1.data);
+				// System.out.println("previous1 = "+previous1.data);
+				next1 = current.next;
+				// If both nodes are adjacent nodes
+				if (next1 != null) {
+					if (next1.data == data2) {
+						next2 = next1.next;
+						if (previous1 != null) {
+							previous1.next = next1;
+						} else {
+							head = next1;
+						}
+						next1.next = current;
+						current.next = next2;
+						return;
+					}
+				}
+			} else if (current.data == data2) {
+				found2 = true;
+				node2 = current;
+				previous2 = previous;
+				System.out.println("Node2  = " + node2.data);
+				// System.out.println("previous2 = "+previous2.data);
+				next2 = current.next;
+				// If both nodes are adjacent nodes
+				if (next2 != null) {
+					if (next2.data == data1) {
+						next1 = next2.next;
+						if (previous2 != null) {
+							previous2.next = next2;
+						} else {
+							head = next2;
+						}
+						next2.next = current;
+						current.next = next1;
+						return;
+					}
+				}
+			}
+			previous = current;
+			current = current.next;
+		}
+		if (found1 && found2) {
+			if (previous1 != null) {
+				previous1.next = node2;
+			} else {
+				head = node2;
+			}
+			node2.next = next1;
+			if (previous2 != null) {
+				previous2.next = node1;
+			} else {
+				head = node1;
+			}
+			node1.next = next2;
+		}
 	}
 
 }
--- src/com/sarindev/ds/linkedlist/Single_Linked_List.java
+++ src/com/sarindev/ds/linkedlist/Single_Linked_List.java
@@ -15,17 +15,27 @@ public class Single_Linked_List {
 		linkedList.insertAtFirst("prashant3");
 		linkedList.insertAtFirst("prashant4");
 
-		
-		try {
-			linkedList.insertAt(2, "Dev");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+//		
+//		try {
+//			linkedList.insertAt(2, "Dev");
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		linkedList.printAll();
+//		linkedList.delete("prashant3");
+//		System.out.println("after deletion : ");
+//		linkedList.printAll();
+		System.out.println("After reversing : ");
+		linkedList.reverse();
 		linkedList.printAll();
-		linkedList.delete("prashant3");
-		System.out.println("after deletion : ");
+//		System.out.println("Nth number from last :");
+//		System.out.println(linkedList.nFromLast(2));
+//		System.out.println("Length of LinkedList = "+linkedList.getLength());
+//		System.out.println("contains \"prashant2\" = "+linkedList.contains("prashant2"));
+		linkedList.swap("prashant1", "prashant4");
+		System.out.println("after swap");
 		linkedList.printAll();
 	}
 
